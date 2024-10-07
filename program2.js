508b29d2f1f2580cfdda93f69577dd9bbbe0cab9
sanskarrai88 <sanskarrai88@gmail.com>
2024-10-07 14:15:23 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,7 +1,30 @@
 const decodeTheRing = function (s, p) {
+    const m = message.length;
+  const p = pattern.length;
 
-    // write your code here
+  const dp = Array(m + 1)
+    .fill(false)
+    .map(() => Array(p + 1).fill(false));
 
+  dp[0][0] = true;
+
+  for (let j = 1; j <= p; j++) {
+    if (pattern[j - 1] === "*") {
+      dp[0][j] = dp[0][j - 1];
+    }
+  }
+
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= p; j++) {
+      if (pattern[j - 1] === message[i - 1] || pattern[j - 1] === "?") {
+        dp[i][j] = dp[i - 1][j - 1];
+      } else if (pattern[j - 1] === "*") {
+        dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+      }
+    }
+  }
+
+  return dp[m][p];
   };
   
   module.exports = decodeTheRing;
